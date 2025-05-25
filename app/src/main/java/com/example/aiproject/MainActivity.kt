71418c4c5970ed2c67926a56fe4fbd0f7340c2e8
Magomed Allahverdiev <magomed.allahverdiev@kupibilet.ru>
2025-05-25 23:42:44 +0300

    создагние проекта

---
+++ app/src/main/java/com/example/aiproject/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.aiproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.aiproject.ui.compose.components.SplashScreen
+import com.example.aiproject.ui.theme.AIProjectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AIProjectTheme {
+                var showSplash by remember { mutableStateOf(true) }
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    if (showSplash)
+                        SplashScreen { showSplash = false }
+                    else Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AIProjectTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/aiproject/ui/compose/components/SplashScreen.kt
@@ -0,0 +1,38 @@
+package com.example.aiproject.ui.compose.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.aiproject.R
+import com.example.aiproject.ui.theme.AIProjectTheme
+import kotlinx.coroutines.delay
+
+@Composable
+internal fun SplashScreen(onFinish: () -> Unit) {
+    LaunchedEffect(Unit) {
+        delay(2000)
+        onFinish()
+    }
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.splash_screen_image),
+            contentDescription = "Splash Screen Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun SplashScreenPreview() {
+    AIProjectTheme {
+        SplashScreen(onFinish = {})
+    }
+}
